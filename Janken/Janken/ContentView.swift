d7a85d4798d1bfea868891c8a9478e9f0a335ab7
Yoshiki <villager2525@gmail.com>
2021-02-14 23:02:59 +0900

    ランダムに画像を表示

--- Janken/Janken/ContentView.swift
+++ Janken/Janken/ContentView.swift
@@ -8,26 +8,47 @@
 import SwiftUI
 
 struct ContentView: View {
-
+    
+    @State var answerNumber:Int = 0
+    
     var body: some View {
-        
         VStack{
-            //画像を指定
-            Image("gu")
-                //リサイズ指定
-                .resizable()
-                //画面内に収まるように､アスペクト比を指定
-                .aspectRatio(contentMode: .fit)
-                .padding()
-            //じゃんけんの種類を指定
-            Text("グー")
-                .padding()
-            Button(action: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/{}/*@END_MENU_TOKEN@*/) {
+            if answerNumber == 0{
+                Text("これからじゃんけんをします")
+            }else if answerNumber == 1{
+                //1はグー
+                Image("gu")
+                    //リサイズ指定
+                    .resizable()
+                    //画面内に収まるように､アスペクト比を指定
+                    .aspectRatio(contentMode: .fit)
+            }else if answerNumber == 2{
+                //2はチョキ
+                Image("choki")
+                    //リサイズ指定
+                    .resizable()
+                    //画面内に収まるように､アスペクト比を指定
+                    .aspectRatio(contentMode: .fit)
+            }else {
+                //それ以外はパー
+                Image("pa")
+                    //リサイズ指定
+                    .resizable()
+                    //画面内に収まるように､アスペクト比を指定
+                    .aspectRatio(contentMode: .fit)
+            }
+            Button(action: {
+                //Buttonがタップされたときの動き
+                print("TAP")
+                //次のじゃんけんへ
+                answerNumber = Int.random(in: 1...3)
+            }) {
+                //Buttonに表示する文字を指定
                 Text("じゃんけんをする!")
                     .foregroundColor(Color.white)
                     .padding()
                     .background(/*@START_MENU_TOKEN@*//*@PLACEHOLDER=View@*/Color.red/*@END_MENU_TOKEN@*/)
-            }
+            }//[じゃんけんをする]ボタンここまで
         }
     }
 }
