d4fabc55f86ab55b1a2c866c390ebdfbaf8a91a0
Yoshiki <villager2525@gmail.com>
2021-02-15 22:29:54 +0900

    first commit

---
+++ Musical/Musical/MusicalApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicalApp.swift
+//  Musical
+//
+//  Created by 西谷恭紀 on 2021/02/15.
+//
+
+import SwiftUI
+
+@main
+struct MusicalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
