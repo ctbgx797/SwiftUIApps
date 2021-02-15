025797dc3aaf476841c7a0304be2f93e0144b15c
Yoshiki <villager2525@gmail.com>
2021-02-15 21:31:14 +0900

    繰り返しの結果を出さないように修正

--- Janken/Janken/ContentView.swift
+++ Janken/Janken/ContentView.swift
@@ -37,11 +37,27 @@ struct ContentView: View {
                     //画面内に収まるように､アスペクト比を指定
                     .aspectRatio(contentMode: .fit)
             }
+            
+            
             Button(action: {
                 //Buttonがタップされたときの動き
                 print("TAP")
                 //次のじゃんけんへ
-                answerNumber = Int.random(in: 1...3)
+                //answerNumber = Int.random(in: 1...3)
+                //新しいじゃんけんの結果を一時的に格納する変数
+                var newAnswerNumber = 0
+                //ランダムに結果を出すが､旋回の結果とは異なる場合のみ採用
+                //repeatは繰り返しを意味する
+                repeat{
+                    //1,2,3の変数をランダムに算出(乱数)
+                    newAnswerNumber = Int.random(in: 1...3)
+                    //前回と同じ結果のときは､再度ランダムに数値を出す
+                    //異なる結果のときは､repeatを抜ける
+                    
+                }while answerNumber == newAnswerNumber
+                
+                answerNumber = newAnswerNumber
+                
             }) {
                 //Buttonに表示する文字を指定
                 Text("じゃんけんをする!")
