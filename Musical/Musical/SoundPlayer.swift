4c543564469f9f8f6abcc8b84621f349361d5725
Yoshiki <villager2525@gmail.com>
2021-02-17 23:02:22 +0900

    シンバルの音がなるコードを追加

--- Musical/Musical/SoundPlayer.swift
+++ Musical/Musical/SoundPlayer.swift
@@ -11,8 +11,18 @@ import AVFoundation
 class SoundPlayer: NSObject {
     //シンバルデータの読み込み
     let cymbalData = NSDataAsset(name: "cymbalSound")!.data
-    
     //シンバル用プレイヤーの変数
     var cymbalPlayer: AVAudioPlayer!
-
+    func cymbalPlay() {
+        do{
+            //シンバル用のプレイヤーに､音源データを指定
+            cymbalPlayer = try AVAudioPlayer(data: cymbalData)
+            //シンバルの音源再生
+            cymbalPlayer.play()
+        }catch {
+            print("シンバルでエラーが発生")
+        }
+        
+    }
+    
 }
