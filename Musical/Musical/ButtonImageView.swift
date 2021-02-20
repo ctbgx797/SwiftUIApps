1d5dcf0551e7e8db47b1e16885346f6ae8f1afee
Yoshiki <villager2525@gmail.com>
2021-02-20 19:00:58 +0900

    共通化するViewの作成

---
+++ Musical/Musical/ButtonImageView.swift
@@ -0,0 +1,20 @@
+//
+//  ButtonImageView.swift
+//  Musical
+//
+//  Created by 西谷恭紀 on 2021/02/20.
+//
+
+import SwiftUI
+
+struct ButtonImageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ButtonImageView_Previews: PreviewProvider {
+    static var previews: some View {
+        ButtonImageView()
+    }
+}
