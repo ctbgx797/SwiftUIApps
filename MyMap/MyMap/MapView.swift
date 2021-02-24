68b264315a61b9d258a1a46cd1dffe8738f2e3fc
Yoshiki <villager2525@gmail.com>
2021-02-24 22:53:38 +0900

    緯度経度確認

--- MyMap/MyMap/MapView.swift
+++ MyMap/MyMap/MapView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 import MapKit
 
 struct MapView: UIViewRepresentable {
-
+    
     //検索キーワード
     let searchKey:String
     
@@ -24,11 +24,31 @@ struct MapView: UIViewRepresentable {
         
         //入力された文字をデバックエリアに表示
         print(searchKey)
+        
+        //CLCLGeocoderインスタンス化
+        let geocoder = CLGeocoder()
+        
+        //入力された文字から位置情報を取得
+        geocoder.geocodeAddressString(
+            searchKey,
+            completionHandler: {(placemarks,error) in
+                
+                //リクエストの結果が存在し､1件目の情報から位置情報を取り出す
+                if let unwrapPlacemarks = placemarks,
+                   let firstPlacemark = unwrapPlacemarks.first,
+                   let location = firstPlacemark.location {
+                    
+                    //位置情報から緯度経度をtargetCoordinateに取り出す
+                    let targetCoordinate = location.coordinate
+                    
+                    //緯度経度をデバックエリアに表示
+                    print(targetCoordinate)
+                }
+            })
     }
-}
-
-struct MapView_Previews: PreviewProvider {
-    static var previews: some View {
-        MapView(searchKey: "東京タワー")
+    
+    struct MapView_Previews: PreviewProvider {
+        static var previews: some View {
+            MapView(searchKey: "東京タワー")
+        }
     }
-}
